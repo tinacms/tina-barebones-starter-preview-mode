c950f49d569b69395341507815bd54fcb5e69ba6
Logan Anderson <landerson2@upei.ca>
2022-11-23 14:51:14 -0500

    Initial commit from Create Tina App

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <Component {...pageProps} />
+  );
+};
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+import { Layout } from "../components/Layout";
+import { useTina } from "tinacms/dist/react";
+import { client } from "../.tina/__generated__/client";
+
+export default function Home(props) {
+  // data passes though in production mode and data is updated to the sidebar data in edit-mode
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+
+  const content = data.page.body;
+  return (
+    <Layout>
+      <TinaMarkdown content={content} />
+    </Layout>
+  );
+}
+
+export const getStaticProps = async () => {
+  const { data, query, variables } = await client.queries.page({
+    relativePath: "home.mdx",
+  });
+
+  return {
+    props: {
+      data,
+      query,
+      variables,
+      //myOtherProp: 'some-other-data',
+    },
+  };
+};
